445c9a306048df2104acde7102e626b7c8407eae
Shubh Raheja <shubh42003@gmail.com>
2024-10-09 20:36:56 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen"
+
+export default function Home() {
+  return(
+    <div className="text-rose-500">
+      <AuthScreen />
+    </div>
+  )
+}
---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { SignInFlow } from "../types";
+
+export const AuthScreen = () => {
+    const [state, setState] = useState<SignInFlow>("signIn")
+    
+    return(
+        <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+            AuthScreen 
+        </div>
+    )
+}
